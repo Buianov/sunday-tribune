6dba75ebde579777ab1c3371d9f16cbc2465a250
Buianov Arkadiy <buianov@mail.ru>
2019-10-15 19:19:07 +0300

    Added filter and show more

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
@@ -7,17 +7,61 @@ import { push } from 'connected-react-router';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const HomePage = ({ posts, users, push }) => {
+  const blankFilter = {
+    name: '',
+    author: '',
+  };
+  const [articlesCount, setArticlesCount] = useState(10);
+  const [filter, setFilter] = useState(blankFilter);
+  const [filteredArticles, setFilteredArticles] = useState([]);
+
+  useEffect(() => {
+    setFilteredArticles(posts);
+  }, [posts]);
+
+  const showMore = () => {
+    if (articlesCount < posts.length) {
+      setArticlesCount(articlesCount + 10);
+    }
+  };
+
   const gotoArticle = id => () => {
     push(`/article/${id}`);
   };
 
+  const filterHandler = e => {
+    setFilter({
+      ...filter,
+      [e.target.id]: e.target.value,
+    });
+    setFilteredArticles(
+      posts.filter(el => {
+        if (e.target.id === 'name') {
+          return el.title.includes(e.target.value);
+        }
+      }),
+    );
+    console.log(filteredArticles);
+  };
+
   return (
     <>
-      {posts && users && posts.length > 0 && (
+      <div>
+        <TextField
+          id="name"
+          label="Search..."
+          value={filter.name}
+          onChange={filterHandler}
+          margin="normal"
+        />
+      </div>
+      {users && filteredArticles && (
         <List>
-          {posts.slice(0, 10).map(({ id, title, userId }) => {
+          {filteredArticles.slice(0, articlesCount).map(({ id, title, userId }) => {
             let userName = users.find(el => el.id === userId).name;
             let text = title.length < 30 ? title : title.slice(0, 30) + '...';
             return (
@@ -26,6 +70,11 @@ const HomePage = ({ posts, users, push }) => {
               </ListItem>
             );
           })}
+          {articlesCount < filteredArticles.length && (
+            <Button variant="contained" onClick={showMore}>
+              Show more...
+            </Button>
+          )}
         </List>
       )}
     </>
